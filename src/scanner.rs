67c17e16e44b6a2beb92a5424ef61a18fb59744b
Max <maxime.rambosson@gmail.com>
2026-03-06 00:18:08 +0100

    Add identifier and reserved keyword in scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
 use crate::token::Token;
 use crate::token::TokenType;
 
@@ -14,6 +17,30 @@ pub struct Scanner {
     start: usize,
     current: usize,
     line: usize,
+}
+
+lazy_static! {
+    static ref KEYWORDS: HashMap<String, TokenType> = {
+        let mut m = HashMap::new();
+        m.insert("and".to_string(), TokenType::And);
+        m.insert("class".to_string(), TokenType::Class);
+        m.insert("else".to_string(), TokenType::Else);
+        m.insert("false".to_string(), TokenType::False);
+        m.insert("for".to_string(), TokenType::For);
+        m.insert("fun".to_string(), TokenType::Fun);
+        m.insert("if".to_string(), TokenType::If);
+        m.insert("nil".to_string(), TokenType::Nil);
+        m.insert("or".to_string(), TokenType::Or);
+        m.insert("print".to_string(), TokenType::Print);
+        m.insert("return".to_string(), TokenType::Return);
+        m.insert("super".to_string(), TokenType::Super);
+        m.insert("this".to_string(), TokenType::This);
+        m.insert("true".to_string(), TokenType::True);
+        m.insert("var".to_string(), TokenType::Var);
+        m.insert("while".to_string(), TokenType::While);
+
+        m
+    };
 }
 
 impl Scanner {
@@ -50,6 +77,26 @@ impl Scanner {
             Some(c) => return c,
             None => panic!("Error: scanner::advance out-of-range"),
         }
+    }
+
+    fn identifier(&mut self) {
+        while Scanner::is_alphanumeric(self.peek()) {
+            self.advance();
+        }
+
+        let text = &self.source[self.start..self.current];
+        match KEYWORDS.get(text) {
+            Some(kind) => self.add_token(kind.clone()),
+            _ => self.add_token(TokenType::Identifier(text.to_string())),
+        }
+    }
+
+    fn is_alpha(c: char) -> bool {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    fn is_alphanumeric(c: char) -> bool {
+        return Scanner::is_digit(c) || Scanner::is_alpha(c);
     }
 
     fn is_digit(c: char) -> bool {
@@ -179,6 +226,8 @@ impl Scanner {
             _ => {
                 if Scanner::is_digit(c) {
                     self.number()
+                } else if Scanner::is_alpha(c) {
+                    self.identifier();
                 } else {
                     self.add_error("Unexpected lexeme".to_string());
                 }

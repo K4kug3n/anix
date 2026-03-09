f03363d047713ddc9c3bc828c1cf5ce17d254dc9
Max <maxime.rambosson@gmail.com>
2026-03-09 23:03:02 +0100

    Implement basic Expr
    - Implement Expr as enum
    - Add basic test for pretty print

---
+++ src/expr.rs
@@ -0,0 +1,91 @@
+use std::fmt;
+
+use crate::token::{Token, TokenType};
+
+pub enum Object {
+    Num(f64),
+    String(String),
+    Bool(bool),
+    Nil,
+}
+
+pub enum Expr {
+    Binary {
+        left: Box<Expr>,
+        op: Token,
+        right: Box<Expr>,
+    },
+    Grouping(Box<Expr>),
+    Literal(Object),
+    Unary {
+        op: Token,
+        right: Box<Expr>,
+    },
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", print_ast(self))
+    }
+}
+
+fn parenthesize(name: &str, exprs: Vec<&Expr>) -> String {
+    let mut result = "(".to_string() + name;
+    for expr in exprs {
+        result += " ";
+        result += print_ast(expr).as_str();
+    }
+    result += ")";
+
+    result
+}
+
+pub fn print_ast(expr: &Expr) -> String {
+    match expr {
+        Expr::Binary { left, op, right } => {
+            return parenthesize(&op.lexeme, vec![left, right]);
+        }
+        Expr::Grouping(inside) => {
+            return parenthesize("group", vec![inside]);
+        }
+        Expr::Literal(literal) => match literal {
+            Literal::String(v) => v.to_string(),
+            Literal::Num(v) => v.to_string(),
+            Literal::Bool(v) => v.to_string(),
+            Literal::Nil => "Nil".to_string(),
+        },
+        Expr::Unary { op, right } => {
+            return parenthesize(&op.lexeme, vec![right]);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::token::TokenType;
+
+    #[test]
+    fn test_print() {
+        let expression = Expr::Binary {
+            left: Box::new(Expr::Unary {
+                op: Token {
+                    kind: TokenType::Minus,
+                    lexeme: "-".to_string(),
+                    line: 1,
+                },
+                right: Box::new(Expr::Literal(Object::Num(123.0))),
+            }),
+            op: Token {
+                kind: TokenType::Star,
+                lexeme: "*".to_string(),
+                line: 1,
+            },
+            right: Box::new(Expr::Grouping(Box::new(Expr::Literal(Object::Num(45.67))))),
+        };
+
+        let result = print_ast(&expression);
+
+        assert_eq!(result, "(* (- 123) (group 45.67))");
+    }
+}
